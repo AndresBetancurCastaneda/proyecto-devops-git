15a9ddeb73b4994f5e7792d69082c99c02646a84
Andres-Betancur-Castañeda <ac20061234@gmail.com>
2026-06-10 07:39:31 -0500

    Actualizar vista principal en rama feature-home

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $usuarios = json_decode(file_get_contents("data/usuarios.json"), true);
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CRUD</title>
+    <title>Proyecto CRUD de Devops</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container mt-5">
